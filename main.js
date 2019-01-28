b0670368f2e35e3bb21373cb44ea1be7e3fd3a93
julien_ripet <ripetjulien@gmail.com>
2019-01-28 16:53:38 +0100

    Deletes old comments, adds bonus in js comments

--- main.js
+++ main.js
@@ -10,6 +10,14 @@
 //     Afficher dans la console les lettre devinés
 //     Le jeu doit savoir quand la partie est finie et féliciter le joueur
 
+
+// Bonus: Se rapprocher du vrais pendu :
+
+//     Gardez en mémoire toutes les lettres devinées (bonnes et mauvaises) et seulement laisser l'utilisateur deviner une lettre une fois.
+//     Si une lettre est entrée deux fois, ne rien faire.
+//     compter le nombre de tentatives ratés
+
+
 //-----------------------------------------------------------------------------------------------------------------//
 
 let arrayWordToGuess = ['B','O','N','J','O','U','R'];
